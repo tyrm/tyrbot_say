739767b001f3a64d1622749bdc633b22a3ff4884
Wolfy Meier <wolfy@wlfy.it>
2016-02-21 19:28:23 -0800

    test say script

---
+++ send.js
@@ -0,0 +1,18 @@
+var config = require('./config');
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect(config.amqp, function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'say';
+
+    var sayObj = {
+      say: 'Hello World.'
+    };
+
+    ch.assertQueue(q, {durable: false});
+    ch.sendToQueue(q, new Buffer(JSON.stringify(sayObj)));
+    console.log(" [x] Sent 'Hello World!'");
+  });
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
